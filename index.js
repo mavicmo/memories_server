280b26a6036e23b175da70ab1a58eb698589868e
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.home>
2023-01-10 22:24:17 -0500

    updated server

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-import express from 'express';
-import bodyParser from 'bodyParser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "bodyParser";
+import mongoose from "mongoose";
+import cors from "cors";
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://mtaraq:mtaraq@cluster0.eqoyi.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
