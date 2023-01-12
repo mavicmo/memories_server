f5052f7e630772124dbfb68d083c6bbfbe0e148e
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.home>
2023-01-11 20:26:33 -0500

    added routes and model

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
-import bodyParser from "bodyParser";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -11,13 +15,13 @@ app.use(cors());
 
 const CONNECTION_URL =
   "mongodb+srv://mtaraq:mtaraq@cluster0.eqoyi.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
+
+mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-
-mongoose.set("useFindAndModify", false);
